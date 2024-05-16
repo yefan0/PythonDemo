1907012f2d45979231b1ea0b1c63edcdbfb58e1e
yefan0 <wyf18516028919@outlook.com>
2024-05-17 04:11:28 +0800

    main

--- newcase/baseprogram.py
+++ newcase/baseprogram.py
@@ -2,3 +2,13 @@
 a = 10
 b = 20
 print ( a + b )
+
+# 变量名不能用数字开头
+# 变量不能是python的关键字
+
+
+name = 'wang'
+age = '18'
+sex = 'man'
+
+# def = 99
---
+++ newcase/inputandprint.py
@@ -0,0 +1,19 @@
+# 输出
+# name = 'wang'
+# age = '18'
+
+# print(name, age)
+
+# 输入
+# 运行程序时可以让用户输入
+
+# name = input('请输入你的名字：')
+# print("您输入的名字是：", name)
+
+# case
+
+user = input('请输入你的账号：')
+password = input('请输入你的密码：')
+
+print(user , password)
+
