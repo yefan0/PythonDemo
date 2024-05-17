5619339b72c5bad5e8eef3007cb9f8cbb10da1dd
yefan0 <wyf18516028919@outlook.com>
2024-05-17 11:16:31 +0800

    main

---
+++ newcase/pythontypefunction.py
@@ -0,0 +1,36 @@
+a:int = 100
+b:int = 20 
+c:int = 6
+# 算数运算
+
+print( a + b )
+print( a - b )
+print( a * b )
+print( a / b )
+
+# 取余数
+print( a % c)
+
+# 取模
+print( a // c )
+
+
+# 比较
+print ( 10 > 7 )
+print ( 90 > 100 )
+
+# bool 的 true false
+res1 = True
+res2 = False
+print (True + 1 + True)
+# True 表示条件成立 --->  1
+# False 表示不成立 --->   0
+print(2 + False)
+
+
+# python 中所有的数据都可以转换为布尔值
+print("================================")
+print(bool('123'),bool(''),123,bool(0)) # 非0为true
+
+
+# 数据的值为0   数据的内容长度为0   数据为空
